4aadd7dd3bdda2eddb3bcf8ba8380f33f0ce3ba8
pucjers <ivanbutrij@gmail.com>
2024-12-04 06:51:58 +0200

    Add edit voice logic

--- db/queries.go
+++ db/queries.go
@@ -3,6 +3,8 @@ package db
 import (
 	"database/sql"
 	"fmt"
+
+	"github.com/lib/pq"
 )
 
 type Voice struct {
@@ -11,6 +13,8 @@ type Voice struct {
 	Description string
 }
 
+// The function `GetUserVoices` retrieves voices associated with a specific user from a database using
+// a provided SQL query.
 func GetUserVoices(db *sql.DB, userID int64) ([]Voice, error) {
 	query := `SELECT id, name, description FROM voices WHERE author_id = $1`
 	rows, err := db.Query(query, userID)
@@ -31,10 +35,17 @@ func GetUserVoices(db *sql.DB, userID int64) ([]Voice, error) {
 	return voices, nil
 }
 
+// The function `SaveVoiceToDB` saves voice data to a database table.
 func SaveVoiceToDB(db *sql.DB, voicePath, name, description string, tags []string, author string, authorID int64) error {
 	query := `
 		INSERT INTO voices (voice_path, name, description, tags, author, author_id)
 		VALUES ($1, $2, $3, $4, $5, $6)`
-	_, err := db.Exec(query, voicePath, name, description, tags, author, authorID)
+	_, err := db.Exec(query, voicePath, name, description, pq.Array(tags), author, authorID)
+	return err
+}
+
+func UpdateVoiceField(db *sql.DB, voiceID int, fieldName string, value interface{}) error {
+	sqlStatement := fmt.Sprintf(`UPDATE voices SET %s = $1 WHERE id = $2`, fieldName)
+	_, err := db.Exec(sqlStatement, value, voiceID)
 	return err
 }
